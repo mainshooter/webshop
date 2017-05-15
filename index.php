51cee167685faeab59436cf720cc3b3184c8b9d4
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-15 15:08:25 +0200

    basic layout page index

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+  <title>title</title>
+
+  <link rel="stylesheet" href="style/grid.css" type="text/css">
+  <link rel="stylesheet" href="style/style.css" type="text/css">
+
+  <script src="js/main.js"></script>
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="row">
+        <nav>
+          <div id="nav" class="col-12">
+              <ul id="width-ul">
+                  <li><a href="">Home</a></li>
+                  <li><a href="#">Producten</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><img class="shopping_card" src="file/site/shopping-cart.png"/></li>
+              </ul>
+          </div>
+        </nav>
+
+        <content>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </content>
+
+        <div class="footer">
+          testing
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
