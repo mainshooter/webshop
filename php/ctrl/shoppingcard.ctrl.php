3947c082a426345db2c1306121f2148ded123133
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-18 17:07:55 +0200

    Shoppingcard ctrl

---
+++ php/ctrl/shoppingcard.ctrl.php
@@ -0,0 +1,25 @@
+<?php
+
+  if (ISSET($_REQUEST['shoppingcard'])) {
+    require '../classes/shoppingcard.class.php';
+    // require '../classes/webshop.class.php';
+    $shoppingcard = new shoppingcard();
+
+    switch ($_REQUEST['shoppingcard']) {
+      case 'add':
+        // Add product from shoppingcard
+        $shoppingcard->add($_REQUEST['productID'], $_REQUEST['amount']);
+        break;
+      case 'delete':
+        // Remove product form shoppingcard
+        $shoppingcard->delete($_REQUEST['productID']);
+        break;
+      case 'update':
+        // Updates the amount of a product
+        $shoppingcard->update($_REQUEST['productID'], $_REQUEST['amount']);
+        break;
+    }
+  }
+
+
+?>
