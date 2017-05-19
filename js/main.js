0a63d625706be7126689d109d935ee5c712112bb
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-19 12:54:11 +0200

    AJAX function added to update a product and to display the shopping card when there is a update

--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
-function shopcard(method, id) {
-  // This function
-}
-var result;
+var shoppingcard;
 (function() {
   shoppingcard = {
     add: function(productID) {
-      result = shoppingcard.ajax("php/ctrl/shoppingcard.ctrl.php?shoppingcard=add&productID=" + productID + "&amount=1");
-      console.log(result);
+      var result = shoppingcard.ajax("php/ctrl/shoppingcard.ctrl.php?shoppingcard=add&productID=" + productID + "&amount=1");
       shoppingcard.count();
     },
   count: function() {
     // This function gets all products from the shoppingcard counts the row - php
     // Displays it
-    result = shoppingcard.ajax("php/ctrl/shoppingcard.ctrl.php?shoppingcard=count");
-    console.log(shoppingcard.ajax("php/ctrl/shoppingcard.ctrl.php?shoppingcard=count"));
+    var result = shoppingcard.ajax("php/ctrl/shoppingcard.ctrl.php?shoppingcard=count");
     document.getElementById('shoppingcardCount').innerHTML = result;
     // $('shoppingcardCount').innerHTML = result.responseText;
   },
+  remove: function(productID) {
+    // Removes a item from the shoppingcard
+  },
+  update: function(productID, amount) {
+    // Update the amount of one product in the shoppingcard
+    shoppingcard.ajax("php/ctrl/shoppingcard.ctrl.php?shoppingcard=update&productID=" + productID + "&amount=" + amount + "");
+    shoppingcard.display();
+  },
+  display: function() {
+    // Displays the shoppingcard after a update
+    var result = shoppingcard.ajax("php/ctrl/shoppingcard.ctrl.php?shoppingcard=display");
+    document.getElementById('content').innerHTML = result;
+  }
   ajax: function(url) {
+    // AJAX SYNC GET REQUEST
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, false);
     xhttp.send();
