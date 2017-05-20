c2c4304b9734ed372a20eb946c0db5d2c9c11cb7
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-20 18:26:07 +0200

    Removed pagenering

--- assets/footer.php
+++ assets/footer.php
@@ -1,17 +1,3 @@
-<div class="pagenering col-12">
-  <?php
-    require_once 'php/classes/product.class.php';
-    require_once 'php/classes/view.class.php';
-    $product = new Product();
-    $productsTotal = $product->countAllProducts();
-
-    $pages = ceil($productsTotal / 10);
-    $view = new View();
-    echo $view->createPagenering($pages);
-
-  ?>
-</div>
-
 <div class="footer col-12">
     &copy;Multiversum - webshop
 </div>
