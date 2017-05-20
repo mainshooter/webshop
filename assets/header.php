644f3524fd55b89128437e078cc0eb84a77bd7d9
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-20 18:35:27 +0200

    Tried to fix some responsive tablet

--- assets/header.php
+++ assets/header.php
@@ -4,13 +4,11 @@
 
  ?>
 <nav>
-    <div class="col-1">
       <a href="index.php">
-          <img class="col-12" src="file/site/logo_nt.png">
+          <img class="col-1 col-m-3" src="file/site/logo_nt.png">
       </a>
-    </div>
-    <div class="col-2 title">Multiversum</div>
-        <div id="nav" class="col-8">
+    <div class="col-2 col-m-9 title">Multiversum</div>
+        <div id="nav" class="col-8 col-m-8">
             <ul id="width-ul">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="products.php">Producten</a></li>
@@ -18,7 +16,7 @@
             </ul>
         </div>
         <a href="winkelmandje.php">
-          <i class="fa fa-shopping-basket col-1 shopping_card" aria-hidden="true"><span id="shoppingcardCount">0</span></i>
+          <i class="fa fa-shopping-basket col-1 col-m-1 shopping_card" aria-hidden="true"><span id="shoppingcardCount">0</span></i>
         </a>
     </nav>
 
