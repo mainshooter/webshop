0d9dcd886bfe1a61845efd00414a5beabefa4537
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-20 18:21:45 +0200

    Added pagenering style

--- assets/footer.php
+++ assets/footer.php
@@ -1,3 +1,17 @@
+<div class="pagenering col-12">
+  <?php
+    require_once 'php/classes/product.class.php';
+    require_once 'php/classes/view.class.php';
+    $product = new Product();
+    $productsTotal = $product->countAllProducts();
+
+    $pages = ceil($productsTotal / 10);
+    $view = new View();
+    echo $view->createPagenering($pages);
+
+  ?>
+</div>
+
 <div class="footer col-12">
     &copy;Multiversum - webshop
 </div>
