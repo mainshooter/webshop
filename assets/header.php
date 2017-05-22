d5328cbd3236adfb6506d074dd5eb05a88cd0fbd
marksalet <marksalet@gmail.com>
2017-05-22 09:07:30 +0200

    Mistakes were made

--- assets/header.php
+++ assets/header.php
@@ -19,3 +19,5 @@
           <i class="fa fa-shopping-basket col-1 col-m-1 shopping_card" aria-hidden="true"><span id="shoppingcardCount">0</span></i>
         </a>
     </nav>
+
+    <img class="col-12 image_header" src="file/site/header.jpg"/>
