8d78a7369f72da6acee768ddf7d9bbbca4441b0e
marksalet <marksalet@gmail.com>
2017-05-22 10:58:43 +0200

    Centering the products in winkelmandje

--- php/classes/view.class.php
+++ php/classes/view.class.php
@@ -59,11 +59,13 @@
         <div class="col-12 product">
           <h2 class="col-12 left-text">' . $key['naam'] . '</h2>
           <img class="col-2" src="' . $key['pad'] . $key['filenaam'] . '">
-          <p class="col-3 left-text winkelmandje-height-center">&euro;' . $key['prijs'] . '</p>';
-          $view .= $this->generateOptionNumbers($key['idProduct'], $amount);
+          <div class="col-10 winkelmandje-height-center">
+            <p class="col-3 left-text">&euro;' . $key['prijs'] . '</p>';
+            $view .= $this->generateOptionNumbers($key['idProduct'], $amount);
 
-          $view .= '<p>Totaal: &euro;' . $productTotal . '</p>
-          <i class="fa fa-trash-o col-1 winkelmandje-height-center" aria-hidden="true" onclick="shoppingcard.remove(' . $key['idProduct'] . ')"></i>
+            $view .= '<i class="fa fa-trash-o col-2" aria-hidden="true" onclick="shoppingcard.remove(' . $key['idProduct'] . ')"></i>
+            <p class="col-4">Totaal: &euro;' . $productTotal . '</p>
+          </div>
         </div>
         ';
       }
@@ -74,7 +76,7 @@
       // Generates a select input field.
       // When the option is the same to the number we got for the option input
       // We set that as selected
-      $selectField = '<select onchange="shoppingcard.update(\'' . $productID . '\', this.value);" class="col-2 winkelmandje-height-center">';
+      $selectField = '<select onchange="shoppingcard.update(\'' . $productID . '\', this.value);" class="col-3">';
       for ($i=0; $i < 10; $i++) {
         if ($amount == $i) {
           $selectField .= '<option value="' . $i .'" selected>' . $i . '</option>';
