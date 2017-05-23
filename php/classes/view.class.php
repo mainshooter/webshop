b3031f23136ec7dd141be0ca111b2137d301c963
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-23 10:25:33 +0200

    Fixed product quantity for shopping card

--- php/classes/view.class.php
+++ php/classes/view.class.php
@@ -101,11 +101,10 @@
       $view = '';
       foreach ($result as $key) {
         $view .= '
-          <div class="col-12"></div>
           <div class="col-2">&nbsp;</div>
           <div class="col-8 product winkelmandje-height-center">
             <img class="col-1" src="' . $key['pad'] . $key['filenaam'] . '">
-            <h2 class="col-6 left-text">' . $key['naam'] . '</h2>
+            <h2 class="col-5 left-text">' . $key['naam'] . '</h2>
             <p class="col-1 left-text">&euro;' . $key['prijs'] . '</p>';
             $view .= $this->generateOptionNumbers($key['idProduct'], $amount);
             $view .= '<i class="fa fa-trash-o col-1" aria-hidden="true" onclick="shoppingcard.remove(' . $key['idProduct'] . ')"></i>
@@ -121,7 +120,7 @@
       // Generates a select input field.
       // When the option is the same to the number we got for the option input
       // We set that as selected
-      $selectField = '<select onchange="shoppingcard.update(\'' . $productID . '\', this.value);" class="col-1">';
+      $selectField = '<select style="margin-top: 2.1em;" onchange="shoppingcard.update(\'' . $productID . '\', this.value);" class="col-2">';
       for ($i=0; $i < 10; $i++) {
         if ($amount == $i) {
           $selectField .= '<option value="' . $i .'" selected>' . $i . '</option>';
